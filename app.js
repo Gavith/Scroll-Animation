052432984da308f558c8405afdfeab85dc497497
Gavith <gavithgethminkaliinux@gmail.com>
2024-09-28 08:23:09 +0530

    fix the buges

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const boxes = document.querySelectorAll('.box');
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', check_boxes);
     console.log(window.innerHeight);
-});
+
+check_boxes();
+
+function check_boxes(){
+    const trigger_bottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach((box) => {
+        const box_top = box.getBoundingClientRect().top;
+
+        if(box_top < trigger_bottom){
+            box.classList.add('show');
+        }else{
+            box.classList.remove('show');
+        }
+    });
+}
+
